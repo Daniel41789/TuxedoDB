e6622aa0bd06e1f47b9be380a16d0a6f134469bf
Daniel41789 <89538726+Daniel41789@users.noreply.github.com>
2024-02-08 23:37:49 -0600

    Delete tuxedo directory

--- tuxedo/inc/biblioBD.fml.h
+++
@@ -1,7 +0,0 @@
-/*	fname	fldid            */
-/*	-----	-----            */
-#define	OUTPUT	((FLDID32)167772165)	/* number: 5	 type: string */
-#define	CODIGO	((FLDID32)33554442)	/* number: 10	 type: long */
-#define	AUTOR	((FLDID32)167772180)	/* number: 20	 type: string */
-#define	LIBRO	((FLDID32)167772190)	/* number: 30	 type: string */
-#define	DESC	((FLDID32)100663336)	/* number: 40	 type: float */
